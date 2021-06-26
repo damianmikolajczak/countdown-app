ff1f334faf1f36195964a7c7a0c253082d9ff1a1
damianmikolajczak <damian_mikolajczak@icloud.com>
2021-06-26 15:31:24 +0200

    Added notification manager description

--- Countdown counter/LocalNotificationMenager.swift
+++ Countdown counter/LocalNotificationMenager.swift
@@ -10,6 +10,7 @@ import UIKit
 import UserNotifications
 
 struct LocalNotificationManager {
+    // This function creates a notification request at a specific time which depends on the events tag. First we get the current notification center.
     static func createNotification(for event: Event) {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .badge, .alert], completionHandler: { granted, error in
@@ -18,10 +19,14 @@ struct LocalNotificationManager {
             }
         })
         
+        // The next step is to create a mutable content with a title and a body that shows the event tame and the time that is left.
         let content = UNMutableNotificationContent()
         content.title = "Something is near the corner."
-        content.body = "You have an upcoming event called \(event.name) in \(event.tag)!"
+        content.body = "You have an upcoming event called \(event.name) in \(event.tag)!" //the event.tag is a calculated property that returns a string with the time that is left.
         
+        // The notification triggers on a specific date which is calculated in the event itself.
+        // Because we need the exact date we have to get the year, month, day, hour, minute and second component from the notification date.
+        // The alst thing to do is to add to the center a request with the created trigger and content.
         let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: event.notificationDate)
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
